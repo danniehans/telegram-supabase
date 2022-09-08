1556b61add108541f8ecd507924078ced716306d
Daniel <danniehans@users.noreply.github.com>
2022-09-08 16:08:16 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.11.0/mod.ts";
+import { format } from "https://deno.land/std@0.154.0/datetime/mod.ts";
+
+
+
 
 const bot = new Bot(Deno.env.get("BOT_TOKEN") ?? "");
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()}`));
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${Intl.DateTimeFormat('en-GB',{timeZone: 'Singapore', dateStyle: 'full', timeStyle: 'long' }).format(new Date())}`));
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
